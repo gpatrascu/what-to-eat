d51eb9bcf9ba1c28fe9f6c82aca8b1077f29128e
Patrascu <george.patrascu@centric.eu>
2021-11-05 23:10:42 +0200

    a

---
+++ az/create_static_web_app.ps1
@@ -0,0 +1,8 @@
+az staticwebapp create `
+    -n what_to_eat `
+    -g what_to_eat `
+    -s https://github.com/gpatrascu/what-to-eat `
+    -l WestEurope `
+    -b main `
+    --app-location "build" `
+    --token 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
